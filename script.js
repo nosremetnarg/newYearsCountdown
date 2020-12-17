a96b486544462b01b1280be7c895efdd6831e7d0
Grant Emerson <grant.alex.emerson@gmail.com>
2020-12-17 10:33:21 -0600

    adding in matchstick asset

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ const loading = document.getElementById("loading");
 const subtitle = document.getElementById("subtitle");
 const audio = new Audio("campFireBurning.mp3");
 const audio1 = new Audio("Campfire-sound.mp3");
+const btn = document.getElementById('btn')
 
 const body = document.getElementById("body");
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
-const christmasShow = new Date(`December 13 ${currentYear} 19:00:00`);
+const christmasShow = new Date(`December 11 ${currentYear} 19:00:00`);
 
 // update countdown time
 function updateCountdown() {
@@ -40,6 +41,7 @@ function updateCountdown() {
     seconds.innerHTML = 0;
 
     subtitle.innerText = "Happening now!";
+    btn.style.display = "inline";
   }
 }
 
@@ -72,22 +74,67 @@ document.addEventListener("click", function () {
   } else {
     stopAudio();
   }
-  console.log("Happy Holidays");
+  console.log("The fire sounds nice doesn't it?");
 });
 
-// function togglePlay() {
-//   isPlaying ? body.pause() : body.play();
-// };
+const d = new Date();
+console.log(d);
 
-// body.onplaying = function() {
-//   isPlaying = true;
-// };
-// body.onpause = function() {
-//   isPlaying = false;
-// };
+const localTime = d.getTime();
+console.log(localTime);
+const localOffset = d.getTimezoneOffset() * 60000;
+console.log(localOffset);
+function getSameCountdown( ) {
+var d = new Date();
 
-// document.addEventListener("click", function () {
-//   togglePlay();
-//   audio.volume = 0.2;
-//   console.log("can you hear the fire? ");
-// });
+var localTime = d.getTime();
+
+var localOffset = d.getTimezoneOffset() * 60000;
+
+var utc = localTime + localOffset;
+// obtain and add destination's UTC time offset
+// for example, Bombay 
+// which is UTC + 5.5 hours
+var offset = 5.5;   
+var bombay = utc + (3600000*offset);
+console.log(`${bombay} bombay`);
+}
+getSameCountdown()
+
+document.body.addEventListener('click', function (evt) {
+  if (evt.target.className === 'firePlace') {
+    if (!isPlaying) {
+      playAudio();
+      audio.volume = 0.2;
+    } else {
+      stopAudio();
+    }
+    console.log("The fire sounds nice doesn't it?");
+  }
+});
+
+document.body.addEventListener('click', function (evt) {
+  if (evt.target.className === 'firePlace' && !isPlaying) {
+      playAudio();
+      audio.volume = 0.2;
+    } else {
+      stopAudio();
+    }
+    console.log("The fire sounds nice doesn't it?");
+  });
+
+  document.body.addEventListener('click', function (evt) {
+    if (evt.target.className === 'firePlace' && !isPlaying) {
+        playAudio();
+        audio.volume = 0.2;
+      } else {
+        stopAudio();
+      }
+      console.log("The fire sounds nice doesn't it?");
+    });
+
+    document.getElementById('firePlace').addEventListener('click', function (evt) {
+ 
+      console.log("you clicked on the fireplace")
+ }
+   );
